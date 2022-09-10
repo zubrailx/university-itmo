747b2caec74127559c2138824cece2e9891c31fb
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-09-10 23:10:11 +0300

    ai: (lab-2) Just writing simple parser with static type checks

--- Year-3/Artificial-intelligence/lab-2/src/main.cpp
+++ Year-3/Artificial-intelligence/lab-2/src/main.cpp
@@ -1,70 +1,70 @@
 #include <fstream>
 #include <iostream>
-#include <iterator>
 #include <sstream>
-#include <string>
-#include <string_view>
+#include <tuple>
+#include <unordered_map>
 #include <vector>
 
-using namespace std;
+using std::cout;
+using std::endl;
 
-std::vector<std::string> getNextLineAndSplitIntoTokens(std::istream &str) {
-  std::vector<std::string> result;
-  std::string line;
-  std::getline(str, line);
+class CSVParser {
+private:
+	std::string m_filename;
+	std::ifstream m_file;
 
-  std::stringstream lineStream(line);
-  std::string cell;
-
-  while (std::getline(lineStream, cell, ',')) {
-    result.push_back(cell);
-  }
-  // This checks for a trailing comma with no data after it.
-  if (!lineStream && cell.empty()) {
-    // If there was a trailing comma then add an empty element.
-    result.push_back("");
-  }
-  return result;
-}
-
-class CSVRow {
 public:
-  std::string_view operator[](std::size_t index) const {
-    return std::string_view(&m_line[m_data[index] + 1],
-                            m_data[index + 1] - (m_data[index] + 1));
-  }
-  std::size_t size() const { return m_data.size() - 1; }
-  void readNextRow(std::istream &str) {
-    std::getline(str, m_line);
+	CSVParser(const std::string & file_name)
+			: m_filename(file_name), m_file(std::ifstream(file_name)) {}
 
-    m_data.clear();
-    m_data.emplace_back(-1);
-    std::string::size_type pos = 0;
-    while ((pos = m_line.find(',', pos)) != std::string::npos) {
-      m_data.emplace_back(pos);
-      ++pos;
-    }
-    // This checks for a trailing comma with no data after it.
-    pos = m_line.size();
-    m_data.emplace_back(pos);
-  }
+	CSVParser(std::string file_name)
+			: m_filename(file_name), m_file(std::ifstream(file_name)) {}
+
+	CSVParser(const char * file_name)
+			: m_filename(file_name), m_file(std::ifstream(file_name)) {}
+
+	~CSVParser() { m_file.close(); }
+
+	template<typename... Args>
+	std::vector<std::tuple<Args...>> parse_file() {
+		std::vector<std::tuple<Args...>> data;
+		std::string line;
+		while (m_file >> line) {
+			std::stringstream ss(line);
+			data.push_back(parse_line<Args...>(ss));
+		}
+		return data;
+	}
 
 private:
-  std::string m_line;
-  std::vector<int> m_data;
-};
+	template<typename... Args>
+	std::tuple<Args...> parse_line(std::stringstream & line_stream) {
+		return std::tuple<Args...>{read_argument<Args>(line_stream)...};
+	}
 
-std::istream &operator>>(std::istream &str, CSVRow &data) {
-  data.readNextRow(str);
-  return str;
-}
+	template<typename T, std::enable_if_t<!std::is_convertible_v<T, std::string>,
+																				bool> = true>
+	T read_argument(std::stringstream & line_stream) {
+		T arg;
+		line_stream >> arg;
+		return arg;
+	}
+
+	template<typename T,
+					 std::enable_if_t<std::is_convertible_v<T, std::string>, bool> = true>
+	T read_argument(std::stringstream & line_stream) {
+		T string;
+		getline(line_stream, string, ';');
+		return string;
+	}
+}; // CSVParser
 
 // Variant: 2 (С.Петербург -> Житомир)
 int main() {
-  std::ifstream file("test/in.1");
-
-  CSVRow row;
-  while (file >> row) {
-    std::cout << "4th Element(" << row[3] << ")\n";
-  }
+	CSVParser parser("test/in.1");
+	auto data = parser.parse_file<std::string, std::string, int>();
+	for (auto line : data) {
+		auto [from, to, dist] = line;
+		cout << from << to << dist << endl;
+	}
 }
