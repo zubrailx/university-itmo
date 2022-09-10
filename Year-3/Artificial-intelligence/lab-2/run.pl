de7288249067fd3969bc725b610c06ba94ababce
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-09-10 12:53:54 +0300

    ai: Add copy lab-1 -> lab-2

---
+++ Year-3/Artificial-intelligence/lab-2/run.pl
@@ -0,0 +1,19 @@
+:- forall(male(X), (write(X), nl)).
+:- nl. % new line
+
+/* OUTPUT
+grigoriy
+vasiliy
+alexei
+oleg
+anatoliy
+nikita
+evgeniy
+iliya
+maksim
+dmitriy
+alexander
+vadim
+vladimir
+*/
+% vim:ft=prolog
