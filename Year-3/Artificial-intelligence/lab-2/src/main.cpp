06aba7172eefc2dbf486f39bf3d4ddc7b1df0f83
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-09-11 11:03:07 +0300

    ai: (lab-2) Copy-paste utility to get type

--- Year-3/Artificial-intelligence/lab-2/src/main.cpp
+++ Year-3/Artificial-intelligence/lab-2/src/main.cpp
@@ -1,17 +1,17 @@
-#include "csv_parser.hpp"
-
 #include <iostream>
+
+#include "csv_parser.hpp"
+#include "utility.hpp"
 
 using std::cout;
 using std::endl;
-
 
 // Variant: 2 (С.Петербург -> Житомир)
 int main() {
 	CSVParser parser("test/in.1");
 	auto data = parser.parse_file<std::string, std::string, int>();
-	for (auto line : data) {
-		auto [from, to, dist] = line;
+	for (const auto & line : data) {
+		const auto & [from, to, dist] = line;
 		cout << from << to << dist << endl;
 	}
 }
