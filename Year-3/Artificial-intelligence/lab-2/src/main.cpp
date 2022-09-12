83181a49f8bfa87c1df8f2395b2147f9701759e9
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-09-12 09:30:15 +0300

    ai: (lab-2) Add TreePrinter

--- Year-3/Artificial-intelligence/lab-2/src/main.cpp
+++ Year-3/Artificial-intelligence/lab-2/src/main.cpp
@@ -2,16 +2,40 @@
 
 #include "csv_parser.hpp"
 #include "utility.hpp"
+#include "tree_printer.hpp"
 
-using std::cout;
-using std::endl;
+void test_tree();
 
 // Variant: 2 (С.Петербург -> Житомир)
 int main() {
-	CSVParser parser("test/in.1");
-	auto data = parser.parse_file<std::string, std::string, int>();
-	for (const auto & line : data) {
-		const auto & [from, to, dist] = line;
-		cout << from << to << dist << endl;
-	}
+	// CSVParser parser("test/in.1");
+	// auto data = parser.parse_file<std::string, std::string, int>();
+	// for (const auto & line : data) {
+	// 	const auto & [from, to, dist] = line;
+	// 	std::cout << from << to << dist << std::endl;
+	// }
+  test_tree();
+}
+
+void test_tree() {
+  TreePrinter tp(std::cout);
+  tp.step_in(2);
+  tp.append("level-2(1)");
+  tp.append("level-2(2)");
+  tp.step_out();
+  tp.append("level-1");
+  tp.step_in(2);
+  tp.append("level-3");
+  tp.step_out();
+  tp.append("level-2");
+  tp.step_out();
+  tp.append("level-1");
+  tp.step_out();
+  auto v = tp.append("level-0");
+  int res = tp.insert_under("level-under-0", v);
+  res = tp.insert_under("level-under-0", v);
+  res = tp.insert_under("level-under-0", v);
+  res = tp.insert_under("level-under-0", v);
+  std::cout << res << std::endl;
+  tp.print();
 }
---
+++ Year-3/Artificial-intelligence/lab-2/src/tree_printer.hpp
@@ -0,0 +1,117 @@
+#include <fstream>
+#include <utility>
+#include <vector>
+
+class TreePrinter {
+private:
+	int m_last = 0;
+	int m_level = 0;
+	std::ostream & m_stream;
+	const std::string M_TREE_SIGNS[6] = {"├", "─", "│", " ", "└", "┐"};
+
+	struct Tree {
+		std::vector<std::pair<std::string, Tree *>> arr;
+	};
+	Tree root;
+
+public:
+	TreePrinter(std::ostream & stream) : m_stream(stream) {}
+
+	void step_in(int step = 1) { m_level += step; }
+
+	void step_out(int step = 1) {
+		m_level -= step;
+		m_level = std::max(0, m_level);
+	}
+
+	std::vector<int> append(const std::string & str) {
+		std::vector<int> stack;
+		auto * arr = &root.arr;
+		for (int i = 0; i < m_level && i < m_last; ++i) {
+			// invariant - size > 0
+			int offset = arr->size() - 1;
+			stack.push_back(offset);
+			arr = &(*arr)[offset].second->arr;
+		}
+
+		if (m_level <= m_last) {
+			stack.push_back(arr->size());
+			arr->emplace_back(str, new Tree);
+		} else {
+			int idx = m_last;
+
+			if (arr->empty()) {// in case root is empty
+				stack.push_back(0);
+				arr = &arr->emplace_back(M_TREE_SIGNS[5], new Tree).second->arr;
+				idx += 1;
+			}
+			for (; idx < m_level; ++idx) {
+				stack.push_back(arr->size());
+				arr = &arr->emplace_back(M_TREE_SIGNS[5], new Tree).second->arr;
+			}
+			stack.push_back(arr->size());
+			arr->emplace_back(str, new Tree);
+			m_last = idx;
+		}
+		return stack;
+	}
+
+	int insert_under(const std::string & str, std::vector<int> & pos) {
+		auto * arr = &root.arr;
+		for (size_t i = 0; i < pos.size(); ++i) {
+			arr = &(*arr)[pos[i]].second->arr;
+		}
+		arr->emplace_back(str, new Tree);
+		return arr->size() - 1;
+	}
+
+	void print() {
+		std::vector<int> helper;
+		print_helper(&root, 0, helper);
+	}
+
+private:
+	void print_helper(const Tree * subtree, int level, std::vector<int> & stack) {
+		stack.push_back(0);
+		const auto & arr = subtree->arr;
+		if (arr.size()) {
+			// Process first
+			print_full_line(subtree, 0, stack, level);
+			// Process [1; N - 2]
+			for (size_t i = 1; i < arr.size() - 1; ++i) {
+				print_full_line(subtree, i, stack, level);
+			}
+			// Process last
+			if (arr.size() > 1) {
+				print_full_line(subtree, arr.size() - 1, stack, level);
+			}
+		}
+		stack.pop_back();
+	}
+
+	void print_full_line(const Tree * subtree, int idx, std::vector<int> & stack,
+											 int level) {
+		stack[level] = idx;
+		print_line(&root, stack, level);
+		m_stream << subtree->arr[idx].first << std::endl;
+		print_helper(subtree->arr[idx].second, level + 1, stack);
+	}
+
+	void print_line(const Tree * root, const std::vector<int> & stack,
+									int level) {
+		auto * arr = &root->arr;
+		for (int i = 0; i < level; ++i) {
+			if (arr->size() - stack[i] - 1) {
+				m_stream << M_TREE_SIGNS[2] << M_TREE_SIGNS[3] << M_TREE_SIGNS[3];
+			} else {
+				m_stream << M_TREE_SIGNS[3] << M_TREE_SIGNS[3] << M_TREE_SIGNS[3];
+			}
+			arr = &(*arr)[stack[i]].second->arr;
+		}
+		if (arr->size() > 1 && arr->size() - stack[level] - 1) {
+			m_stream << M_TREE_SIGNS[0] << M_TREE_SIGNS[1] << M_TREE_SIGNS[1];
+		} else {
+			m_stream << M_TREE_SIGNS[4] << M_TREE_SIGNS[1] << M_TREE_SIGNS[1];
+		}
+	}
+};
