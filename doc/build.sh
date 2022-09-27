50b3b95c1d4ebf79229662b21e2b1e664a4b5e74
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-09-27 19:08:11 +0300

    Update titlepage

--- doc/build.sh
+++ doc/build.sh
@@ -6,7 +6,8 @@ SCRIPT_DIR=`dirname $0`
 cd $SCRIPT_DIR
 
 # Idk, just some shitty errors when launching for the first time
-latexmk -shell-escape -xelatex -interaction=nonstopmode -outdir=build/ main.tex || \
-latexmk -shell-escape -xelatex -interaction=nonstopmode -outdir=build/ main.tex
+latexmk -pdf -shell-escape -xelatex -interaction=nonstopmode -outdir=build/ main.tex || \
+latexmk -pdf -shell-escape -xelatex -interaction=nonstopmode -outdir=build/ main.tex
+mv build/main.pdf ./
 
 cd $CURRENT_DIR
