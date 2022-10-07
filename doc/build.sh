6528ae9034387ed7d9c0523261bb1f20c8525cc3
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-10-07 14:00:00 +0300

    Add images

--- doc/build.sh
+++ doc/build.sh
@@ -6,8 +6,5 @@ SCRIPT_DIR=`dirname $0`
 cd $SCRIPT_DIR
 
 # Idk, just some shitty errors when launching for the first time
-latexmk -pdf -shell-escape -xelatex -interaction=nonstopmode -outdir=build/ main.tex || \
-latexmk -pdf -shell-escape -xelatex -interaction=nonstopmode -outdir=build/ main.tex
+latexmk -pdf -shell-escape -xelatex -outdir=build/ -interaction=nonstopmode main.tex
 mv build/main.pdf ./
-
-cd $CURRENT_DIR
