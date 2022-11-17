bd969f099e4c7e4850d2dfd8a1a4e9a9d79e4534
Nikita Kulakov <Nikitka.Kulakov@yandex.ru>
2022-11-17 12:23:08 +0300

    os.dop: Fix print format

--- Year-3/Operating-systems/dop/uefi-app/main.c
+++ Year-3/Operating-systems/dop/uefi-app/main.c
@@ -8,24 +8,25 @@ efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
   UINTN MapSize, MapKey;
   UINTN DescriptorSize;
   UINT32 DescriptorVersion;
-  EFI_MEMORY_DESCRIPTOR* Map = NULL;
+  EFI_MEMORY_DESCRIPTOR *Map = NULL;
 
-  SystemTable->BootServices->GetMemoryMap(&MapSize, Map, &MapKey, &DescriptorSize, &DescriptorVersion);
-  SystemTable->BootServices->AllocatePool(EfiLoaderData, MapSize, (void **)&Map);
-  SystemTable->BootServices->GetMemoryMap(&MapSize, Map, &MapKey, &DescriptorSize, &DescriptorVersion);
-
+  SystemTable->BootServices->GetMemoryMap(&MapSize, Map, &MapKey,
+                                          &DescriptorSize, &DescriptorVersion);
+  SystemTable->BootServices->AllocatePool(EfiLoaderData, MapSize,
+                                          (void **)&Map);
+  SystemTable->BootServices->GetMemoryMap(&MapSize, Map, &MapKey,
+                                          &DescriptorSize, &DescriptorVersion);
 
   UINTN MmapEntries = MapSize / DescriptorSize;
   for (UINTN i = 0; i < MmapEntries; ++i) {
-    EFI_MEMORY_DESCRIPTOR *Desc = (EFI_MEMORY_DESCRIPTOR *)((UINTN) Map + (i * DescriptorSize));
-    Print(L"Type=%d, ", Desc->Type);
-    Print(L"Attr=%d, ", Desc->Attribute);
-    Print(L"Size=%d, ", Desc->NumberOfPages * EFI_PAGE_SIZE);
-    Print(L"PhysStart=%d, ", Desc->PhysicalStart);
-    Print(L"VirtStart=%d, ", Desc->VirtualStart);
-    Print(L"Pad=%d\n", Desc->Pad);
+    EFI_MEMORY_DESCRIPTOR *Desc =
+        (EFI_MEMORY_DESCRIPTOR *)((UINTN)Map + (i * DescriptorSize));
+    Print(L"Type=%u, ", Desc->Type);
+    Print(L"PhysStart=%lu, ", (UINT64)Desc->PhysicalStart);
+    Print(L"VirtStart=%lu, ", (UINT64)Desc->VirtualStart);
+    Print(L"Size=%lu, ", (UINT64)Desc->NumberOfPages * EFI_PAGE_SIZE);
+    Print(L"Attr=%lX\n", (UINT64)Desc->Attribute);
   }
   SystemTable->BootServices->FreePool((void *)Map);
   return EFI_SUCCESS;
 }
-
