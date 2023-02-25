602b0bfb0b2f1e0a880d61d7297f025a74f62c49
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-02-25 20:01:02 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Main().getGreeting());
+    }
+}
