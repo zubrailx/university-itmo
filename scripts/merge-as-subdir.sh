84445e96e29be8050616dc4343aee82e87a11e82
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-03-06 12:33:28 +0300

    Add script to merge another repos as subdirs

---
+++ scripts/merge-as-subdir.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -u # Don't allow executing with empty variable
+set -e # Exit when command fails
+
+REMOTE_NAME=$1
+REMOTE_URL=$2
+REMOTE_BRANCH=$3
+REPO_PREFIX=$4
+
+PREV_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Fetching
+git remote add $REMOTE_NAME $REMOTE_URL
+git fetch $REMOTE_NAME $REMOTE_BRANCH
+
+git switch -c merge-$REMOTE_NAME
+git merge -s ours --no-commit --allow-unrelated-histories $REMOTE_NAME/$REMOTE_BRANCH
+git read-tree --prefix=$REPO_PREFIX -u $REMOTE_NAME/$REMOTE_BRANCH
+git commit -m "Merge $REMOTE_NAME/$REMOTE_BRANCH as subdir"
+
+git pull -s subtree $REMOTE_NAME $REMOTE_BRANCH
+
+git checkout $PREV_BRANCH
