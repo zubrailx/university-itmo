fdb2a4e0c33d528af07fd65bd6c19edd07ceaa26
Valerii Butorin <butorin.v.a@mail.ru>
2023-03-19 22:51:24 +0300

    # fixes

--- lab-2.srcs/sim_1/new/counter_tb.v
+++ lab-2.srcs/sim_1/new/counter_tb.v
@@ -1,54 +1,46 @@
 `timescale 1ns / 10ps
- 
+
 module counter_tb;
- 
+
     reg clk;
     reg en;
     reg rst;
     wire[31:0] y;
- 
+
     counter cnt(
         .clk(clk),
         .en(en),
         .rst(rst),
         .out(y)
     );
- 
- 
+
+
     integer i;
     reg[31:0] expected;
     initial begin
         // reset counter before the start
-        rst = 1;
-        #5
-        rst = 0;
-        #5
-        
+        #5 rst = 1;
+        #5 rst = 0;
+
         // start counting
         en = 1;
-        
+
         for(i = 0; i < 32; i = i + 1) begin
             $display ("Current loop#%0d ", i);
             if (i == 16)
-                rst = 1;
-                #5
-                rst = 0;
-                #5
- 
- 
+                #5 rst = 1;
+                #5 rst = 0;
+
             if (i == 30)
-                en = 0;
-                #5
- 
-            clk = 1;
-            #5
-            clk = 0;
-            #5
- 
+                #5 en = 0;
+
+            #5 clk = 1;
+            #5 clk = 0;
+
             if (i == 15) expected = 16;
             else if (i < 30) expected = (i + 1) % 16;
             else expected = 14;
- 
+
             if (expected === y) begin
                 $display("Correct! y=%b", y);
             end else begin
--- lab-2.srcs/sim_1/new/shift_right_tb.v
+++ lab-2.srcs/sim_1/new/shift_right_tb.v
@@ -1,13 +1,13 @@
 `timescale 1ns / 10ps
- 
+
 module shift_right_tb;
- 
+
     reg clk;
     reg en;
     reg rst;
     reg[31:0] x;
     wire carry;
- 
+
     shift_right shr (
         .clk(clk),
         .en(en),
@@ -15,46 +15,39 @@ module shift_right_tb;
         .in(x),
         .carry(carry)
     );
- 
+
     integer i;
     reg [31:0] value = 65536; // on iter 16 should be 1
     reg expected;
     initial begin
         x = value;
-        
+
         rst = 0;
-        en = 0; // load value     
-        clk = 1;
-        #5;
-        clk = 0;
-        #5;
-         
+        en = 0; // load value
+        #5 clk = 1;
+        #5 clk = 0;
+
         en = 1; // start shifting
         for (i = 0; i < 32; i = i + 1) begin
             $display ("Current loop#%0d ", i);
-            
+
             expected = value % 2;
-            
+
             if (i == 20) begin
-                en = 0;
-                #5;
+                #5 en = 0;
             end
-            
+
             if (i == 30) begin
-                rst = 1;
-                #5;
-                rst = 0;
-                #5;
+                #5 rst = 1;
+                #5 rst = 0;
             end
-            
-            clk = 1;
-            #5
-            clk = 0;
-            #5;
-             
+
+            #5 clk = 1;
+            #5 clk = 0;
+
             if (i < 20) value = value / 2;
             else if (i >= 30) value = 0;
- 
+
             if (expected == carry) begin
                 $display("Correct! y=%b", carry);
             end else begin
