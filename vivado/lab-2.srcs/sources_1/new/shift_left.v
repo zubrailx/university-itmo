1219189db59a910869f6795d1f302924ef70bdc6
Nikita Kulakov <nvkulakov.dev@gmail.com>
2023-04-03 14:33:25 +0300

    Make parameters scream case

--- vivado/lab-2.srcs/sources_1/new/shift_left.v
+++ vivado/lab-2.srcs/sources_1/new/shift_left.v
@@ -1,22 +1,22 @@
 `timescale 1ns / 1ps
  
 module shift_left
-    #(parameter width = 32)
+    #(parameter WIDTH = 32)
    (input                  clk,
     input                  en,
     input                  rst,
     input                  l,
-    input      [width-1:0] in,
+    input      [WIDTH-1:0] in,
     output reg             out); // carry flag
     
-    reg [width-1:0] data;
+    reg [WIDTH-1:0] data;
  
     always @(posedge clk, posedge rst) begin
         if (rst) {out, data} <= 0;
         else if (l) begin
             data <= in;
         end else if (en)
-            {out, data[width-1:0]} <= {data[width-1:0], 1'b0};
+            {out, data[WIDTH-1:0]} <= {data[WIDTH-1:0], 1'b0};
         else out <= 0;
     end
     
